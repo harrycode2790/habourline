78f0e9e4652c35423d9d3308c9b4ee22465ba9eb
harry <hillarybond10@gmail.com>
2025-03-12 11:46:27 +0000

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
+              app/page.
             </code>
-            .
+            
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          
         </a>
       </footer>
     </div>
