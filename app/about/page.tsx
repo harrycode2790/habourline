654e9932a2aa7821e4ba724e9ea1403ad3c40817
Akpos <iyagaakpos@gmail.com>
2025-03-20 13:10:31 +0100

    Added the next section of the about page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import StatsSection from "@/components/StatsSection";
+import TwoColumnText from "@/components/TwoColumnText";
 
 export default function AboutPage() {
     const stats = [
@@ -8,7 +9,7 @@ export default function AboutPage() {
         { value: "1,035k", title: "Parcels delivered per year" },
         { value: "409", title: "Territories served" },
         ];
-        
+
     return (
         <>
         <HeroSection
@@ -17,6 +18,18 @@ export default function AboutPage() {
             backgroundImage="/images/about-top.png"
         />
         <StatsSection stats= {stats} />
+
+        <TwoColumnText
+            leftText="We are the digital logistics company for Nigeria"
+            rightText={{
+            bold: "We want to lead the way towards sustainable DIGITAL logistics – we strive to run efficient operations by being a highly trusted company and partner that protect the environment, provide an ideal place to work for all.",
+            paragraphs: [
+                "With well-working logistics, your business also runs smoother. This goal guides our everyday actions.",
+                "We are a rapidly growing domestic family business, and one of Finland’s largest businesses in the logistics field. We offer customised transportation, forwarding and storage services as well as versatile delivery chain management solutions everywhere in the world.",
+                "Our workdays are filled with new transportations and logistics services, and new opportunities like your commission. We will handle it carefully and on time, together with our trusted partners."
+            ],
+            }}
+        />
         </>
     );
 }
