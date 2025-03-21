6f5cd13beeb5aaf0bd5b225ec7b3cb04db6df508
Akpos <iyagaakpos@gmail.com>
2025-03-21 15:54:39 +0100

    Added the image and services-list section to the services page.

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 import TwoColumnText from "@/components/TwoColumnText";
+import ServicesList from "./services-list";
 
 export default function ServicesPage() {
 
@@ -9,13 +11,27 @@ export default function ServicesPage() {
             description="We provide customised transportations, forwarding and storage services as well as 
             versatile delivery management solutions everywhere in the world."
             backgroundColor="#1019C2"/>  
+
             <div className="container px-6 py-12 md:py-20 md:px-30">
+
                 <TwoColumnText leftText="Transportation and special services"
                 rightText={{
                     paragraphs: [`Whatever your transportation needs, you can rest assured that we will ensure timely 
                         delivery together with our competent partners, while you focus on your core business.`]
                 }}/>
 
+                <div className="flex justify-center md:mt-12">
+                    <Image 
+                        src="/images/services-image.png" 
+                        alt="Shipping Containers"
+                        width={900}
+                        height={300}
+                        className=" w-full h-auto "
+                    />
+                </div>
+
+                <ServicesList />
+                
             </div>
         </>
     )
