bef29cc9ddcbd7ebd37b16942b71f5fd2d1d22de
harry <hillarybond10@gmail.com>
2025-03-26 10:56:14 +0000

    removed the default favicon and replace it with the orignal one

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Harbourline Shipping",
   description: "Harbourline Shipping Services",
+  icons : [{
+    rel: "icon",
+    url: "/images/favicon2.png",
+    sizes: "64x64",
+  }]
 };
 
 export default function RootLayout({
